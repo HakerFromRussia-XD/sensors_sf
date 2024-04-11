4b4e914227159f92198d5a57e432320ca24b4708
Evrei-XD <30876844+Evrei-XD@users.noreply.github.com>
2024-04-11 18:42:11 +0300

    Создание потока для параллельной обработки данных

--- app/src/main/java/com/example/sensors_sf/MainActivity.java
+++ app/src/main/java/com/example/sensors_sf/MainActivity.java
@@ -1,37 +1,52 @@
 package com.example.sensors_sf;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
-import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
-
     private SensorManager sensorManager;
+    private Sensor sensor;
 
-    private List<Sensor> deviceSensors;
+    private Thread threadSensors = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         textView = findViewById(R.id.textView);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
-        printSensors();
+
+//        textView.setText((int) sensor.getPower() + sensor.getName());
+        refresh();
     }
 
-    private void printSensors() {
-        for (Sensor sensor : deviceSensors){
-            textView.setText(textView.getText()+"\n"+sensor.getName());
-        }
+    private void refresh() {
+        threadSensors = new Thread(() -> {
+            while (true) {
+                runOnUiThread(() -> {
+                    textView.setText((int) sensor.getPower() + sensor.getName());
+                });
+                System.err.println("отправили новые данные getPower: "+sensor.getPower());
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.err.println("случилась ошибка "+e);
+                }
+            }
+        });
+        threadSensors.start();
     }
 }
