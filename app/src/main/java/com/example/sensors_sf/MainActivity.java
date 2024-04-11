767b4081deb360718f0891381dbdf49d55507161
Evrei-XD <30876844+Evrei-XD@users.noreply.github.com>
2024-04-11 18:10:32 +0300

    first commit

---
+++ app/src/main/java/com/example/sensors_sf/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sensors_sf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
